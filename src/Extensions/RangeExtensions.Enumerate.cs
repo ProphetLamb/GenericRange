0c2155a70630021dad0ad8d6b4debc7f28b9dc7e
ProphetLamb <rene.carannante@outlook.de>
2021-06-20 12:08:25 +0200

    Add docs

--- src/Extensions/RangeExtensions.Enumerate.cs
+++ src/Extensions/RangeExtensions.Enumerate.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics.Contracts;
-using System.Runtime.CompilerServices;
 
 namespace GenericRange.Extensions
 {
     public static partial class RangeExtensions
     {
+        
         /// <summary>Enumerates all integers in the <see cref="Range{T}"/>, from start inclusive and end inclusive.</summary>
         /// <param name="range">The range.</param>
         /// <param name="length">The length.</param>
         [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static IEnumerable<long> Enumerate(this Range<long> range, long length)
+        {
+            long start = range.Start.GetOffset(length), end = range.End.GetOffset(length);
+            for (long i = start; i < end; i++)
+                yield return i;
+        }
+
+        /// <summary>Enumerates all integers in the <see cref="Range{T}"/>, from start inclusive and end inclusive.</summary>
+        /// <param name="range">The range.</param>
+        /// <param name="length">The length.</param>
+        [Pure]
+        public static IEnumerable<long> Enumerate(this Range<long> range)
+        {
+            range.AssertNotFromEnd();
+            for (long i = range.Start.Value; i < range.End.Value; i++)
+                yield return i;
+        }
+        
+        /// <summary>Enumerates all integers in the <see cref="Range{T}"/>, from start inclusive and end inclusive.</summary>
+        /// <param name="range">The range.</param>
+        /// <param name="length">The length.</param>
+        [Pure]
         public static IEnumerable<int> Enumerate(this Range<int> range, int length)
         {
             int start = range.Start.GetOffset(length), end = range.End.GetOffset(length);
@@ -20,13 +41,12 @@ namespace GenericRange.Extensions
         
         /// <summary>Enumerates all integers in the <see cref="Range{T}"/>, from start inclusive and end inclusive.</summary>
         /// <param name="range">The range.</param>
-        /// <param name="length">The length.</param>
         [Pure]
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IEnumerable<long> Enumerate(this Range<long> range, long length)
+        public static IEnumerable<int> Enumerate(this Range<int> range)
         {
-            long start = range.Start.GetOffset(length), end = range.End.GetOffset(length);
-            for (long i = start; i < end; i++)
+            range.AssertNotFromEnd();
+            int start = range.Start.Value, end = range.End.Value;
+            for (int i = start; i < end; i++)
                 yield return i;
         }
     }
