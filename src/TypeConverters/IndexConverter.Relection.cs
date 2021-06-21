5000a93c0e1c494f6cfeba9ca8576543d4052b80
ProphetLamb <rene.carannante@outlook.de>
2021-06-21 23:33:09 +0200

    Replace if-else branch with a compiled delegate.

---
+++ src/TypeConverters/IndexConverter.Relection.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Text.Json;
+
+using GenericRange.Utility;
+
+namespace GenericRange.TypeConverters
+{
+    public sealed partial class IndexConverter<T>
+    {
+        internal delegate ReadOnlySpan<char> SerializeDelegate(ref Index<T> index, JsonSerializerOptions options);
+
+        internal delegate T DeserializeDelegate(ref ReadOnlySpan<char> serialized, JsonSerializerOptions options);
+
+        internal static SerializeDelegate Serialize;
+
+        internal static DeserializeDelegate Deserialize;
+
+        private static SerializeDelegate GetSerializeDelegate()
+        {
+            SerializeDelegate serialize;
+            if (IsEnumUnderlying)
+            {
+                serialize = delegate(ref Index<T> index, JsonSerializerOptions options) {
+                    return JsonSerializer.Serialize(index.Value, options).AsSpan()[1..^1];
+                };
+            }
+            else if (IsTimeSpanUnderlying)
+            {
+                serialize = delegate(ref Index<T> index, JsonSerializerOptions options) {
+                    return JsonSerializer.Serialize(index.Value.Cast<TimeSpan>().Ticks, options);
+                };
+            }
+            else
+            {
+                serialize = delegate(ref Index<T> index, JsonSerializerOptions options) {
+                    return JsonSerializer.Serialize(index.Value, options);
+                };
+            }
+            
+            return (SerializeDelegate)Delegate.CreateDelegate(typeof(SerializeDelegate), null, serialize.GetMethodInfo());
+        }
+        
+        private static DeserializeDelegate GetDeserializeDelegate()
+        {
+            DeserializeDelegate deserialize;
+            if (IsEnumUnderlying)
+            {
+                deserialize = delegate(ref ReadOnlySpan<char> serialized, JsonSerializerOptions options) {
+                    var quoted = serialized.Quote(stackalloc char[serialized.Length + 2]);
+                    return options.Deserialize<T>(quoted);
+                };
+            }
+            else if (IsTimeSpanUnderlying)
+            {
+                deserialize = delegate(ref ReadOnlySpan<char> serialized, JsonSerializerOptions options) {
+                    return new TimeSpan(options.Deserialize<long>(serialized)).Cast<T>();
+                };
+            }
+            else
+            {
+                deserialize = delegate(ref ReadOnlySpan<char> serialized, JsonSerializerOptions options) {
+                    return options.Deserialize<T>(serialized);
+                };
+            }
+
+            return (DeserializeDelegate)Delegate.CreateDelegate(typeof(DeserializeDelegate), null, deserialize.GetMethodInfo());
+        }
+    }
+}
--- src/TypeConverters/IndexConverter.cs
+++ src/TypeConverters/IndexConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text.Json;
 
@@ -14,31 +15,27 @@ namespace GenericRange.TypeConverters
     public sealed partial class IndexConverter<T> : DefaultConverterFactory<Index<T>> where T : unmanaged, IComparable
     {
         internal static bool IsEnumUnderlying { get; } = typeof(T).IsEnum;
-
         internal static bool IsTimeSpanUnderlying { get; } = typeof(T) == typeof(TimeSpan);
-        
         internal static JsonSerializerOptions DefaultOptions { get; } = IsEnumUnderlying ? JsonHelper.EnumOptions : JsonHelper.DefaultOptions;
-        
+
         static IndexConverter()
         {
             _ = new Index<T>(); // Throws type initializer error if invalid generic type.
+
+            Serialize = GetSerializeDelegate();
+            Deserialize = GetDeserializeDelegate();
         }
 
         [Pure]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Index<T> Parse(ReadOnlySpan<char> serialized) => Parse(serialized, DefaultOptions);
+        public static Index<T> Parse(in ReadOnlySpan<char> serialized) => Parse(serialized, DefaultOptions);
 
         [Pure]
-        public static Index<T> Parse(ReadOnlySpan<char> serialized, JsonSerializerOptions options)
+        public static Index<T> Parse(in ReadOnlySpan<char> serialized, JsonSerializerOptions options)
         {
             bool fromEnd = serialized[0] == '^';
-            if (fromEnd)
-                serialized = serialized.Slice(fromEnd ? 1 : 0);
-            if (IsTimeSpanUnderlying)
-                return new Index<T>(new TimeSpan(options.Deserialize<long>(serialized)).Cast<T>(), fromEnd);
-            if (IsEnumUnderlying)
-                return new Index<T>(options.Deserialize<T>(serialized.Quote(stackalloc char[serialized.Length + 2])), fromEnd);
-            return new Index<T>(options.Deserialize<T>(serialized), fromEnd);
+            var valueStr = fromEnd ? serialized.Slice(1) : serialized;
+            return new Index<T>(Deserialize(ref valueStr, options), fromEnd);
         }
 
         [Pure]
@@ -46,16 +43,9 @@ namespace GenericRange.TypeConverters
         public static string ToString(in Index<T> index) => ToString(index, DefaultOptions);
         
         [Pure]
-        public static string ToString(in Index<T> index, JsonSerializerOptions options)
+        public static string ToString(Index<T> index, JsonSerializerOptions options)
         {
-            ReadOnlySpan<char> json;
-            if (IsTimeSpanUnderlying)
-                json = JsonSerializer.Serialize(index.Value.Cast<TimeSpan>().Ticks, options);
-            else if (IsEnumUnderlying)
-                json = JsonSerializer.Serialize(index.Value, options).AsSpan()[1..^1];
-            else
-                json = JsonSerializer.Serialize(index.Value, options);
-            
+            var json = Serialize(ref index, options);
             if (index.IsFromEnd)
                 return "^" + json.ToString();
             return json.ToString();
