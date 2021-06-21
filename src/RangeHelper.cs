0a7bea1fba89d68bdf486e68efc1e3db254549b7
ProphetLamb <rene.carannante@outlook.de>
2021-06-21 23:04:13 +0200

    Improve performance by compiling the subtract delegate. Fix my reflection gibberish.

--- src/RangeHelper.cs
+++ src/RangeHelper.cs
@@ -2,25 +2,107 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading;
 
 namespace GenericRange
 {
     partial struct Index<T>
     {
-        private static readonly Func<object, object, object> s_subtract = RangeHelper.GetGenericSubtractDelegate<T>();
+        private static readonly IndexHelper<T>.Sub s_subtract = IndexHelper<T>.GetSubtractDelegate();
         internal static readonly Comparer<T> s_comparer = Comparer<T>.Default;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal static T Subtract(in T minuend, in T subtrahend) => s_subtract(minuend, subtrahend).Cast<T>();
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        internal static T Subtract(object minuend, object subtrahend) => s_subtract(ref minuend, ref subtrahend);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static int Compare(in T left, in T right) => s_comparer.Compare(left, right);
     }
+    
+    internal static class IndexHelper<T> where T : unmanaged
+    {
+        internal static ThreadLocal<object[]> s_subParams = new(() => new object[2]);
+        
+        internal delegate T Sub(ref object minuend, ref object subtrahend);
+
+        internal static Sub GetSubtractDelegate()
+        {
+            Type t = typeof(T);
+
+            Sub? subDelegate = GetPrimitiveSubtractDelegate(t);
+            
+            if (subDelegate == null && t.IsEnum)
+            {
+                Type underlyingType = t.GetEnumUnderlyingType();
+                subDelegate = GetPrimitiveSubtractDelegate(underlyingType);
+            }
+
+            if (subDelegate != null)
+                return (Sub)Delegate.CreateDelegate(typeof(Sub), null, subDelegate.GetMethodInfo());
+
+            throw new InvalidOperationException();
+        }
+        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static Sub? GetPrimitiveSubtractDelegate(Type t)
+        {
+            if (t == typeof(sbyte))
+                return SubSbyte;
+            if (t == typeof(byte))
+                return SubByte;
+            if (t == typeof(short))
+                return SubShort;
+            if (t == typeof(ushort))
+                return SubUShort;
+            if (t == typeof(int))
+                return SubInt;
+            if (t == typeof(uint))
+                return SubUInt;
+            if (t == typeof(long))
+                return SubLong;
+            if (t == typeof(ulong))
+                return SubULong;
+            if (t == typeof(float))
+                return SubFloat;
+            if (t == typeof(double))
+                return SubDouble;
+            if (t == typeof(decimal))
+                return SubDecimal;
+            if (t == typeof(char))
+                return SubChar;
+            if (t == typeof(TimeSpan))
+                return SubTimeSpan;
+            return null;
+        }
+
+        private static T SubSbyte(ref object minuend, ref object subtrahend) => ((sbyte)(minuend.Cast<sbyte>() - subtrahend.Cast<sbyte>())).Cast<T>();
+
+        private static T SubByte(ref object minuend, ref object subtrahend) => ((byte)(minuend.Cast<byte>() - subtrahend.Cast<byte>())).Cast<T>();
+
+        private static T SubShort(ref object minuend, ref object subtrahend) => ((short)(minuend.Cast<short>() - subtrahend.Cast<short>())).Cast<T>();
+
+        private static T SubUShort(ref object minuend, ref object subtrahend) => ((ushort)(minuend.Cast<ushort>() - subtrahend.Cast<ushort>())).Cast<T>();
+
+        private static T SubInt(ref object minuend, ref object subtrahend) => (minuend.Cast<int>() - subtrahend.Cast<int>()).Cast<T>();
+
+        private static T SubUInt(ref object minuend, ref object subtrahend) => (minuend.Cast<uint>() - subtrahend.Cast<uint>()).Cast<T>();
+
+        private static T SubLong(ref object minuend, ref object subtrahend) => (minuend.Cast<long>() - subtrahend.Cast<long>()).Cast<T>();
+
+        private static T SubULong(ref object minuend, ref object subtrahend) => (minuend.Cast<ulong>() - subtrahend.Cast<ulong>()).Cast<T>();
+
+        private static T SubFloat(ref object minuend, ref object subtrahend) => (minuend.Cast<float>() - subtrahend.Cast<float>()).Cast<T>();
+
+        private static T SubDouble(ref object minuend, ref object subtrahend) => (minuend.Cast<double>() - subtrahend.Cast<double>()).Cast<T>();
+
+        private static T SubDecimal(ref object minuend, ref object subtrahend) => (minuend.Cast<decimal>() - subtrahend.Cast<decimal>()).Cast<T>();
+        
+        private static T SubChar(ref object minuend, ref object subtrahend) => ((char)(minuend.Cast<char>() - subtrahend.Cast<char>())).Cast<T>();
+        
+        private static T SubTimeSpan(ref object minuend, ref object subtrahend) => (minuend.Cast<TimeSpan>() - subtrahend.Cast<TimeSpan>()).Cast<T>();
+    }
 
     internal static class RangeHelper
     {
-        private static readonly Dictionary<Type, Delegate> s_typeSubtractDelegateMap = new();
-        
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         internal static T Cast<T>(this object obj) => (T)obj;
         
@@ -30,64 +112,6 @@ namespace GenericRange
             where TOut : unmanaged
         {
             return (TOut)System.Convert.ChangeType(obj, typeof(TOut));
-        }
-
-        internal static Func<object, object, object> GetGenericSubtractDelegate<T>() where T : unmanaged, IComparable
-        {
-            Type t = typeof(T);
-            var subtractFunction = GetPrimitiveSubtractDelegate(t);
-            if (subtractFunction != null)
-                return subtractFunction;
-            
-            if (t.IsEnum)
-               return GetEnumSubtractDelegate(t);
-            
-            throw new NotSupportedException("The type is not supported. Supported type are sbyte, byte, short, ushort, int, uint, long, ulong, float, double, decimal, char, TimeSpan, or any Enum or Nullable<T> type with one of the aforementioned as underlying type.");
-        }
-
-        private static Func<object, object, object>? GetPrimitiveSubtractDelegate(Type t)
-        {
-            if (t == typeof(sbyte))
-                return (minuend, subtrahend) => (sbyte)(minuend.Cast<sbyte>() - subtrahend.Cast<sbyte>());
-            if (t == typeof(byte))
-                return (minuend, subtrahend) => (byte)(minuend.Cast<byte>() - subtrahend.Cast<byte>());
-            if (t == typeof(short))
-                return (minuend, subtrahend) => (short)(minuend.Cast<short>() - subtrahend.Cast<short>());
-            if (t == typeof(ushort))
-                return (minuend, subtrahend) => (ushort)(minuend.Cast<ushort>() - subtrahend.Cast<ushort>());
-            if (t == typeof(int))
-                return (minuend, subtrahend) => minuend.Cast<int>() - subtrahend.Cast<int>();
-            if (t == typeof(char))
-                return (minuend, subtrahend) => (char)(minuend.Cast<char>() - subtrahend.Cast<char>());
-            if (t == typeof(uint))
-                return (minuend, subtrahend) => minuend.Cast<uint>() - subtrahend.Cast<uint>();
-            if (t == typeof(long))
-                return (minuend, subtrahend) => minuend.Cast<long>() - subtrahend.Cast<long>();
-            if (t == typeof(ulong))
-                return (minuend, subtrahend) => minuend.Cast<ulong>() - subtrahend.Cast<ulong>();
-            if (t == typeof(float))
-                return (minuend, subtrahend) => minuend.Cast<float>() - subtrahend.Cast<float>();
-            if (t == typeof(double))
-                return (minuend, subtrahend) => minuend.Cast<double>() - subtrahend.Cast<double>();
-            if (t == typeof(decimal))
-                return (minuend, subtrahend) => minuend.Cast<decimal>() - subtrahend.Cast<decimal>();
-            if (t == typeof(TimeSpan))
-                return (minuend, subtrahend) => minuend.Cast<TimeSpan>() - subtrahend.Cast<TimeSpan>();
-            return null;
-        }
-
-        private static Func<object, object, object> GetEnumSubtractDelegate(Type t)
-        {
-            Type underlyingType = t.GetEnumUnderlyingType();
-            if (s_typeSubtractDelegateMap.TryGetValue(underlyingType, out Delegate? subtractDelegate))
-                return (Func<object, object, object>)subtractDelegate;
-            MethodInfo getSubtractMethod = typeof(RangeHelper)
-               .GetMethod(nameof(GetGenericSubtractDelegate), BindingFlags.NonPublic | BindingFlags.Static)!
-               .MakeGenericMethod(underlyingType);
-            subtractDelegate = (Delegate)getSubtractMethod.Invoke(null, Array.Empty<object>());
-
-            s_typeSubtractDelegateMap.Add(underlyingType, subtractDelegate);
-            return (Func<object, object, object>)subtractDelegate;
         }
     }
 }
