7a93dda17b8668d64c910611bad3893c662f6b80
ProphetLamb <rene.carannante@outlook.de>
2021-06-21 10:57:28 +0200

    Add extension method Index Index<int>.ToRange

--- src/Extensions/RangeExtensions.cs
+++ src/Extensions/RangeExtensions.cs
@@ -9,6 +9,13 @@ namespace GenericRange.Extensions
         /// </summary>
         /// <param name="range">The range</param>
         /// <returns>The <see cref="Range"/> equivalent to the <paramref name="range"/>.</returns>
-        public static Range ToRange(this Range<int> range) => new(new Index(range.Start.Value, range.Start.IsFromEnd), new Index(range.End.Value, range.End.IsFromEnd));
+        public static Range ToRange(this Range<int> range) => new(range.Start.ToIndex(), range.End.ToIndex());
+        
+        /// <summary>
+        /// Converts the generic index to a integer <see cref="Index"/>.
+        /// </summary>
+        /// <param name="index">The index.</param>
+        /// <returns>The <see cref="Index"/> equivalent to the <paramref name="index"/>.</returns>
+        public static Index ToIndex(this Index<int> index) => new(index.Value, index.IsFromEnd);
     }
 }
